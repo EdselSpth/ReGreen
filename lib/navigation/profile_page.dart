b11d1391ecac8cb92d09558df7348f4b4be03997
tifunnyy <funnyrnd92@gmail.com>
2025-10-28 18:58:52 +0700

    menambahkan profile page

--- lib/navigation/profile_page.dart
+++ lib/navigation/profile_page.dart
@@ -6,8 +6,228 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Home Page')),
+      appBar: AppBar(
+        title: const Text('Profil Pengguna'),
+        backgroundColor: const Color(
+          0xFF7F9F4F,
+        ), // Warna hijau dari desain Figma
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/profile1.jpeg'),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: Column(
+                children: const [
+                  Text(
+                    'Edsel',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Bergabung Sejak 2025',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Informasi Pribadi',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Menggunakan pushReplacement untuk menggantikan halaman tanpa menambah stack
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const EditProfilePage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Edit Profil',
+                    style: TextStyle(color: Colors.green, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            _buildInfoCard(Icons.email, 'Email', 'edselspth@gmail.com'),
+            const SizedBox(height: 16),
+            _buildInfoCard(Icons.phone, 'No. Telepon', '0851-5503-0650'),
+            const SizedBox(height: 16),
+            _buildInfoCard(
+              Icons.location_on,
+              'Alamat',
+              'Kompleks Taman Bumi Prima Blok O No 8, Kecamatan Cibabat, Kota Cimahi 40513',
+            ),
+            const SizedBox(height: 32),
+            _buildInfoCard(Icons.exit_to_app, 'Lainnya', 'Keluar'),
+            const SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoCard(IconData icon, String title, String content) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Icon(icon, color: Colors.black),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    content,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profil Pengguna'),
+        backgroundColor: const Color(0xFF7F9F4F),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/profile1.jpeg'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            _buildEditCard('Nama', 'Edsel'),
+            const SizedBox(height: 16),
+            _buildEditCard('Email', 'edselspth@gmail.com'),
+            const SizedBox(height: 16),
+            _buildEditCard('No Telepon', '0851-5503-0650'),
+            const SizedBox(height: 16),
+            _buildEditCard(
+              'Alamat',
+              'Kompleks Taman Bumi Prima Blok O No 8, Kecamatan Cibabat, Kota Cimahi 40513',
+            ),
+            const SizedBox(height: 16),
+            _buildEditCard('Password', '', isPassword: true),
+            const SizedBox(height: 32),
+            Center(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF7F9F4F),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Simpan Perubahan',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditCard(
+    String label,
+    String initialValue, {
+    bool isPassword = false,
+  }) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              initialValue: initialValue,
+              obscureText: isPassword,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Masukkan $label',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
