a0d05ac9ff27bb64524fee5084381ded4dcb3697
tifunnyy <funnyrnd92@gmail.com>
2025-12-22 22:39:56 +0700

    fix: [FNY] penambahan alert dialog pada penarikan area dan jalan

--- lib/Model/area_model.dart
+++ lib/Model/area_model.dart
@@ -1,20 +1,25 @@
 class AreaModel {
-  final String kecamatan;
+  final int? id;
+  final String jalan;
   final String kelurahan;
+  final String kecamatan;
   final String kota;
   final String provinsi;
 
   AreaModel({
-    required this.kecamatan,
+    this.id,
+    required this.jalan,
     required this.kelurahan,
+    required this.kecamatan,
     required this.kota,
     required this.provinsi,
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'kecamatan': kecamatan,
+      'jalan': jalan,
       'kelurahan': kelurahan,
+      'kecamatan': kecamatan,
       'kota': kota,
       'provinsi': provinsi,
     };
@@ -22,8 +27,10 @@ class AreaModel {
 
   factory AreaModel.fromJson(Map<String, dynamic> json) {
     return AreaModel(
-      kecamatan: json['kecamatan'],
+      id: json['id'],
+      jalan: json['jalan'],
       kelurahan: json['kelurahan'],
+      kecamatan: json['kecamatan'],
       kota: json['kota'],
       provinsi: json['provinsi'],
     );
